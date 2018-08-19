50119e45ac9835fca4545d212a88efd1c4382830
rairai214 <spt85wx9@gmail.com>
2018-08-19 09:07:55 +0900

    build.grable WebsampleApplication.java update

--- src/main/java/com/example/websample/WebsampleApplication.java
+++ src/main/java/com/example/websample/WebsampleApplication.java
@@ -2,11 +2,18 @@ package com.example.websample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebsampleApplication {
+public class WebsampleApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebsampleApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WebsampleApplication.class);
 	}
 }
